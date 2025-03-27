cb7f51e55f6f11c3de1ca9a0f34cead5a21a2378
Anna-Lena <bikrneranna@gmail.com>
2025-03-27 10:27:36 +0100

    latest changes

--- src/assets/styles/contact.js
+++ src/assets/styles/contact.js
@@ -1,7 +1,7 @@
-const form = document.getElementById("form");
-const result = document.getElementById("result");
+const form = document.getElementById('form');
+const result = document.getElementById('result');
 
-form.addEventListener("submit", function (e) {
+form.addEventListener('submit', function (e) {
   const formData = new FormData(form);
   e.preventDefault();
   var object = {};
@@ -9,37 +9,34 @@ form.addEventListener("submit", function (e) {
     object[key] = value;
   });
   var json = JSON.stringify(object);
-  result.innerHTML = "Please wait...";
+  result.innerHTML = 'Please wait...';
 
-  fetch("https://api.web3forms.com/submit", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    body: json
+  fetch('https://api.web3forms.com/submit', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+    body: json,
   })
     .then(async (response) => {
       let json = await response.json();
       if (response.status == 200) {
         result.innerHTML = json.message;
-        result.classList.remove("text-gray-500");
-        result.classList.add("text-green-500");
+        result.classList.remove('text-gray-500');
+        result.classList.add('text-green-500');
       } else {
         console.log(response);
         result.innerHTML = json.message;
-        result.classList.remove("text-gray-500");
-        result.classList.add("text-red-500");
+        result.classList.remove('text-gray-500');
+        result.classList.add('text-red-500');
       }
     })
     .catch((error) => {
       console.log(error);
-      result.innerHTML = "Something went wrong!";
+      result.innerHTML = 'Something went wrong!';
     })
     .then(function () {
       form.reset();
       setTimeout(() => {
-        result.style.display = "none";
+        result.style.display = 'none';
       }, 5000);
     });
 });
